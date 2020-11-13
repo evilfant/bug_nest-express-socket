bdf3244573cf254ccc52b89b52c053b0c1a489fc
tzubairov <tzubairov@yandex-team.ru>
2020-11-13 16:16:19 +0300

    Add variant with http

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express
 import { join } from 'path';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import * as http from 'http';
 
 async function bootstrap() {
   const expressApp = express();
@@ -16,5 +17,7 @@ async function bootstrap() {
 
   expressApp.listen(3000) // if change this line to the next one everything will be fine
   // app.listen(3000);
+
+  // http.createServer(expressApp).listen(3000); // this variant also doesn't work
 }
 bootstrap();
